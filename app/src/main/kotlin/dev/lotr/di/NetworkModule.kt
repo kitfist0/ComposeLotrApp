c0578a416ccc1959b94b7ea5de11b6d28508b881
KitFisto <kitfist0@yandex.ru>
2021-10-21 20:31:33 +0300

    Create dagger network module

---
+++ app/src/main/kotlin/dev/lotr/di/NetworkModule.kt
@@ -0,0 +1,27 @@
+package dev.lotr.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitBuilder(okHttpClient: OkHttpClient): Retrofit.Builder {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+    }
+}
