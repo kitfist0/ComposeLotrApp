1d934683efec6ad09ea92af1a4ee4f5b8aa2b1d0
KitFisto <kitfist0@yandex.ru>
2021-10-26 00:16:36 +0300

    Add dispatcher qualifiers to use cases

--- app/src/main/kotlin/dev/lotr/domain/usecase/GetBookChaptersUseCase.kt
+++ app/src/main/kotlin/dev/lotr/domain/usecase/GetBookChaptersUseCase.kt
@@ -1,11 +1,13 @@
 package dev.lotr.domain.usecase
 
+import dev.lotr.domain.di.IoDispatcher
 import dev.lotr.domain.model.Chapter
 import dev.lotr.domain.repository.BookAndChapterRepository
 import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
 
-class GetBookChaptersUseCase(
-    coroutineDispatcher: CoroutineDispatcher,
+class GetBookChaptersUseCase @Inject constructor(
+    @IoDispatcher coroutineDispatcher: CoroutineDispatcher,
     private val bookAndChapterRepository: BookAndChapterRepository,
 ) : BaseUseCase<String, List<Chapter>>(coroutineDispatcher) {
     override suspend fun execute(parameters: String): List<Chapter> {
--- app/src/main/kotlin/dev/lotr/domain/usecase/GetBooksUseCase.kt
+++ app/src/main/kotlin/dev/lotr/domain/usecase/GetBooksUseCase.kt
@@ -1,11 +1,13 @@
 package dev.lotr.domain.usecase
 
+import dev.lotr.domain.di.IoDispatcher
 import dev.lotr.domain.model.Book
 import dev.lotr.domain.repository.BookAndChapterRepository
 import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
 
-class GetBooksUseCase(
-    coroutineDispatcher: CoroutineDispatcher,
+class GetBooksUseCase @Inject constructor(
+    @IoDispatcher coroutineDispatcher: CoroutineDispatcher,
     private val bookAndChapterRepository: BookAndChapterRepository,
 ) : BaseUseCase<Nothing?, List<Book>>(coroutineDispatcher) {
     override suspend fun execute(parameters: Nothing?): List<Book> {
