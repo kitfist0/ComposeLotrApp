661a7e14859276de89a59548cde474df6e6e3d8d
kitfist0 <kitfist0@yandex.ru>
2021-10-27 16:01:15 +0300

    Refactor books screen and add preview

--- app/src/main/kotlin/dev/lotr/presentation/ui/books/BooksScreen.kt
+++ app/src/main/kotlin/dev/lotr/presentation/ui/books/BooksScreen.kt
@@ -1,46 +1,65 @@
 package dev.lotr.presentation.ui.books
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.insets.statusBarsPadding
 import dev.lotr.domain.model.Book
 
 @Composable
-fun BooksScreen(viewModel: BooksViewModel) {
+fun BooksScreen(
+    viewModel: BooksViewModel,
+    modifier: Modifier = Modifier,
+) {
     val books = viewModel.books.collectAsState()
     Body(
         books = books.value,
-        onItemClick = { bookId -> viewModel.onBookClicked(bookId) },
+        onBookClick = { bookId -> viewModel.onBookClicked(bookId) },
+        modifier,
     )
 }
 
 @Composable
-private fun Body(books: List<Book>, onItemClick: (bookId: String) -> Unit) {
-    Surface(
-        color = MaterialTheme.colors.background,
-        modifier = Modifier.fillMaxSize(),
+private fun Body(
+    books: List<Book>,
+    onBookClick: (String) -> Unit,
+    modifier: Modifier,
+) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = modifier.statusBarsPadding(),
     ) {
-        LazyColumn {
-            for (book in books) {
-                item {
-                    Text(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable { onItemClick.invoke(book.id) }
-                            .padding(16.dp),
-                        text = book.name,
-                    )
-                }
-            }
+        items(books) { book ->
+            Text(
+                text = book.name,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onBookClick.invoke(book.id) }
+                    .padding(16.dp)
+            )
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BooksScreenPreview() {
+    Body(
+        books = listOf(
+            Book("5cf58077b53e011a64671582", "The Fellowship Of The Ring"),
+            Book("5cf58077b53e011a64671583", "The Two Towers"),
+            Book("5cf58077b53e011a64671584", "The Return Of The King"),
+        ),
+        onBookClick = {},
+        modifier = Modifier,
+    )
 }
