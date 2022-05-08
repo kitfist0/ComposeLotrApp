ce19490e0417f440d355acc0dbe91ec140baa6fb
kitfist0 <kitfist0@yandex.ru>
2022-05-08 15:48:51 +0300

    Update gradle dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -14,21 +14,21 @@ object Application {
 
 object Versions {
     object Essential {
-        const val Kotlin = "1.6.0"
+        const val Kotlin = "1.6.10"
         const val Coroutines = "1.6.0"
         const val Serialization = "1.3.2"
-        const val Gradle = "7.0.4"
+        const val Gradle = "7.1.3"
         const val Ktx = "1.7.0"
     }
 
     object Compose {
-        const val Master = "1.1.0-rc01"
-        const val Accompanist = "0.24.0-alpha"
+        const val Master = "1.1.1"
+        const val Accompanist = "0.24.7-alpha"
         const val Activity = "1.4.0"
-        const val Lifecycle = "2.4.0"
-        const val Navigation = "2.4.0-rc01"
-        const val HiltNavigation = "1.0.0-rc01"
-        const val Material3 = "1.0.0-alpha03"
+        const val Lifecycle = "2.4.1"
+        const val Navigation = "2.4.2"
+        const val HiltNavigation = "1.0.0"
+        const val Material3 = "1.0.0-alpha10"
     }
 
     object Network {
@@ -38,11 +38,11 @@ object Versions {
     }
 
     object Di {
-        const val Hilt = "2.40.5"
+        const val Hilt = "2.41"
     }
 
     object Ui {
-        const val Material3 = "1.6.0-alpha02"
+        const val Material3 = "1.6.0"
     }
 
     object Test {
